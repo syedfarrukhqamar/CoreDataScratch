eef308e2b19340feb466ffa1e1e65b4155786bca
syed farrukh Qamar <farrukh.qamar@bemycompetence.se>
2015-12-18 17:28:58 +0100

    Initial Commit

---
+++ CoreDataScratch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataScratch
+//
+//  Created by syed farrukh Qamar on 18/12/15.
+//  Copyright © 2015 Be My Competence AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
